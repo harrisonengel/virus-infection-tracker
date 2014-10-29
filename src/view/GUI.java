e1e2d3c90e717f3446de91536f9c0d53ee816dc9
Harrison Engel <hengel1993@gmail.com>
2014-10-29 22:06:28 +1100

    Creat intuitive prompting for inputing patient info and general outline for upload and save features

---
+++ src/view/GUI.java
@@ -0,0 +1,203 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JFileChooser;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import java.awt.TextArea;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GUI {
+
+	private JFrame frame;
+	private JOptionPane jPane;
+	private String firstName, middleName, lastName, ssn, sex, city, state, dateInfected;
+	private int age;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI(){
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.DARK_GRAY);
+		frame.setBounds(100, 100, 700, 550);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		jPane = new JOptionPane();
+		
+		JPanel panel_header = new JPanel();
+		panel_header.setBackground(Color.GRAY);
+		panel_header.setBounds(0, 0, 684, 156);
+		frame.getContentPane().add(panel_header);
+		panel_header.setLayout(null);
+		
+		JLabel lblTitle = new JLabel("");
+		lblTitle.setBounds(215, -38, 259, 194);
+		lblTitle.setIcon(new ImageIcon(GUI.class.getResource("/view/virus 2.jpg")));
+		panel_header.add(lblTitle);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(GUI.class.getResource("/view/virus3.jpg")));
+		lblNewLabel.setBounds(10, 0, 205, 156);
+		panel_header.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setIcon(new ImageIcon(GUI.class.getResource("/view/virus3.jpg")));
+		lblNewLabel_1.setBounds(474, 0, 200, 156);
+		panel_header.add(lblNewLabel_1);
+		
+		JPanel panel_buttons = new JPanel();
+		panel_buttons.setBackground(Color.DARK_GRAY);
+		panel_buttons.setBounds(10, 167, 209, 345);
+		frame.getContentPane().add(panel_buttons);
+		panel_buttons.setLayout(null);
+		
+		JButton btnAdd = new JButton("ADD DISEASE TREES");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					//getFileName() defined below.
+					String fileName = getFileName(frame);
+					if (fileName != null) {
+						
+					}
+					//These catches are for users who either don't select a file or select a file 
+					//that is in the wrong format (not txt, or not a properly formatted txt.
+				} catch (NullPointerException np) {
+					JOptionPane. showMessageDialog(frame, "Error reading from file. Make sure it exists and is not corrupted!");
+				} catch (NumberFormatException nfp) {
+					JOptionPane. showMessageDialog(frame, "Error reading from file. Make sure it exists and is not corrupted!");
+				}
+			}
+		});
+		btnAdd.setBackground(new Color(0, 255, 0));
+		btnAdd.setBounds(10, 11, 189, 38);
+		panel_buttons.add(btnAdd);
+		
+		
+		JButton btnNewButton = new JButton("ADD PATIENT");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				patientPrompt();
+			}
+		});
+		btnNewButton.setBackground(new Color(0, 255, 0));
+		btnNewButton.setBounds(10, 63, 189, 38);
+		panel_buttons.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("REMOVE PATIENT");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				patientPrompt();
+			}
+		});
+		btnNewButton_1.setBackground(new Color(0, 255, 0));
+		btnNewButton_1.setBounds(10, 117, 189, 38);
+		panel_buttons.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("SAVE TREES");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fc = new JFileChooser();
+				int returnVal = fc.showSaveDialog(frame);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					
+					try (FileWriter fw = new FileWriter(fc.getSelectedFile())) {
+					
+					} catch (IOException i) {
+						JOptionPane. showMessageDialog(frame, "Error saving file. Make sure you are using a .txt extension!");
+					}
+				}
+			}
+		});
+		btnNewButton_2.setBackground(new Color(0, 255, 0));
+		btnNewButton_2.setBounds(10, 172, 189, 38);
+		panel_buttons.add(btnNewButton_2);
+		
+		TextArea textAreaMessages = new TextArea();
+		textAreaMessages.setForeground(new Color(255, 0, 0));
+		textAreaMessages.setBackground(new Color(192, 192, 192));
+		textAreaMessages.setBounds(0, 224, 209, 111);
+		panel_buttons.add(textAreaMessages);
+		
+		JPanel panel_display = new JPanel();
+		panel_display.setBackground(new Color(192, 192, 192));
+		panel_display.setBounds(229, 167, 445, 334);
+		frame.getContentPane().add(panel_display);
+		
+	}
+	
+	/*
+	 * Bug: Throws error message on X.
+	 */
+	private void patientPrompt(){
+		patientPanel prompt = new patientPanel();
+		
+		try{
+			JOptionPane.showConfirmDialog(null, prompt);
+			firstName = prompt.getFirst();
+			lastName = prompt.getLast();
+			middleName = prompt.getMiddle();
+			ssn = prompt.getSsn();
+			age = prompt.getAge();
+			sex = prompt.getGender();
+			city = prompt.getCity();
+			state = prompt.getState();
+			dateInfected = prompt.getDate();
+		} catch (NumberFormatException nFE){
+			//Display error message saying something went terribly wrong!!
+			JOptionPane. showMessageDialog(frame, "Error, incorrect input. Make sure your age is an integer number, not a spelled out number (like \"two\").");
+		}
+		
+	}
+	
+	//Uses a JFileCooser to get a path, which can then be passed into an 
+		//EasyReader object.
+		public String getFileName(JFrame frame) {
+			JFileChooser fc = new JFileChooser();
+			int returnVal = fc.showOpenDialog(frame);
+			if (returnVal == JFileChooser.APPROVE_OPTION){
+				//This message to the user will be replaced
+				//by an error message immediately afterwards
+				//if there is an error reading in the file.
+				return fc.getSelectedFile().getPath();
+			}
+			else {
+				return null;
+			}
+		}
+}
---
+++ src/view/patientPanel.java
@@ -0,0 +1,180 @@
+package view;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JTextField;
+import java.util.InputMismatchException;
+
+public class patientPanel extends JPanel {
+	private JTextField textFieldFirstName;
+	private JTextField textFieldMiddleName;
+	private JTextField textFieldLastName;
+	private JTextField textFieldSsn;
+	private JTextField textFieldAge;
+	private JTextField textFieldGender;
+	private JTextField textFieldCity;
+	private JTextField textFieldState;
+	private JTextField textFieldDate;
+	private JLabel lblInputPatientInfo;
+
+	/**
+	 * Create the panel.
+	 */
+	public patientPanel() {
+		
+		Dimension minimumSize = new Dimension(350, 400);
+		setPreferredSize(new Dimension(350, 400));
+		setMinimumSize(minimumSize);
+		
+		setBackground(Color.DARK_GRAY);
+		setForeground(new Color(0, 255, 0));
+		setLayout(null);
+		
+		JLabel lblFirstName = new JLabel("First Name");
+		lblFirstName.setForeground(new Color(0, 255, 0));
+		lblFirstName.setBackground(new Color(0, 255, 0));
+		lblFirstName.setFont(new Font("DialogInput", Font.BOLD, 14));
+		lblFirstName.setBounds(10, 11, 100, 25);
+		add(lblFirstName);
+		
+		textFieldFirstName = new JTextField();
+		textFieldFirstName.setBounds(140, 12, 175, 25);
+		add(textFieldFirstName);
+		textFieldFirstName.setColumns(10);
+		
+		JLabel lblMiddleName = new JLabel("Middle Name");
+		lblMiddleName.setForeground(new Color(0, 255, 0));
+		lblMiddleName.setFont(new Font("DialogInput", Font.BOLD, 14));
+		lblMiddleName.setBounds(10, 47, 100, 25);
+		add(lblMiddleName);
+		
+		textFieldMiddleName = new JTextField();
+		textFieldMiddleName.setBounds(140, 48, 175, 25);
+		add(textFieldMiddleName);
+		textFieldMiddleName.setColumns(10);
+		
+		JLabel labelLastName = new JLabel("Last Name");
+		labelLastName.setForeground(new Color(0, 255, 0));
+		labelLastName.setFont(new Font("DialogInput", Font.BOLD, 14));
+		labelLastName.setBounds(10, 83, 100, 25);
+		add(labelLastName);
+		
+		JLabel lblSsn = new JLabel("SSN");
+		lblSsn.setForeground(new Color(0, 255, 0));
+		lblSsn.setFont(new Font("DialogInput", Font.BOLD, 14));
+		lblSsn.setBounds(10, 119, 100, 25);
+		add(lblSsn);
+		
+		JLabel lblAge = new JLabel("Age");
+		lblAge.setForeground(new Color(0, 255, 0));
+		lblAge.setFont(new Font("DialogInput", Font.BOLD, 14));
+		lblAge.setBounds(10, 155, 100, 25);
+		add(lblAge);
+		
+		JLabel lblGender = new JLabel("Gender");
+		lblGender.setForeground(new Color(0, 255, 0));
+		lblGender.setFont(new Font("DialogInput", Font.BOLD, 14));
+		lblGender.setBounds(10, 191, 100, 25);
+		add(lblGender);
+		
+		JLabel lblInfectionCity = new JLabel("Infection City");
+		lblInfectionCity.setForeground(new Color(0, 255, 0));
+		lblInfectionCity.setFont(new Font("DialogInput", Font.BOLD, 14));
+		lblInfectionCity.setBounds(10, 227, 118, 25);
+		add(lblInfectionCity);
+		
+		JLabel lblInfectionState = new JLabel("Infection State");
+		lblInfectionState.setForeground(new Color(0, 255, 0));
+		lblInfectionState.setFont(new Font("DialogInput", Font.BOLD, 14));
+		lblInfectionState.setBounds(10, 263, 128, 25);
+		add(lblInfectionState);
+		
+		JLabel lblDate = new JLabel("Date");
+		lblDate.setForeground(new Color(0, 255, 0));
+		lblDate.setFont(new Font("DialogInput", Font.BOLD, 14));
+		lblDate.setBounds(10, 299, 100, 25);
+		add(lblDate);
+		
+		textFieldLastName = new JTextField();
+		textFieldLastName.setColumns(10);
+		textFieldLastName.setBounds(140, 84, 175, 25);
+		add(textFieldLastName);
+		
+		textFieldSsn = new JTextField();
+		textFieldSsn.setColumns(10);
+		textFieldSsn.setBounds(140, 120, 175, 25);
+		add(textFieldSsn);
+		
+		textFieldAge = new JTextField();
+		textFieldAge.setColumns(10);
+		textFieldAge.setBounds(140, 156, 175, 25);
+		add(textFieldAge);
+		
+		textFieldGender = new JTextField();
+		textFieldGender.setColumns(10);
+		textFieldGender.setBounds(140, 192, 175, 25);
+		add(textFieldGender);
+		
+		textFieldCity = new JTextField();
+		textFieldCity.setColumns(10);
+		textFieldCity.setBounds(140, 228, 175, 25);
+		add(textFieldCity);
+		
+		textFieldState = new JTextField();
+		textFieldState.setColumns(10);
+		textFieldState.setBounds(140, 266, 175, 25);
+		add(textFieldState);
+		
+		textFieldDate = new JTextField();
+		textFieldDate.setColumns(10);
+		textFieldDate.setBounds(140, 300, 175, 25);
+		add(textFieldDate);
+		
+		lblInputPatientInfo = new JLabel("INPUT PATIENT INFO");
+		lblInputPatientInfo.setForeground(Color.GREEN);
+		lblInputPatientInfo.setFont(new Font("DialogInput", Font.BOLD, 20));
+		lblInputPatientInfo.setBounds(54, 364, 261, 25);
+		add(lblInputPatientInfo);
+
+	}
+	
+	public String getFirst(){
+		return textFieldFirstName.getText();
+	}
+	public String getLast(){
+		return textFieldLastName.getText();
+	}
+	public String getMiddle(){
+		return textFieldMiddleName.getText();
+	}
+	public String getSsn(){
+		return textFieldSsn.getText();
+	}
+	public int getAge(){ //returns -1 if entered an impossible age, -2 if given a string that cannot be an int
+		try{
+			int age = Integer.parseInt(textFieldAge.getText());
+			if (age >=0 && age < 125) return age;
+			else return -1;
+		} catch (InputMismatchException ime){
+			return -2;
+		}
+	}
+	/*NEED TO FIX TO IDENTIFY IF A CORRECT GENDER WAS ENTERED*/
+	
+	public String getGender(){
+		return textFieldGender.getText();
+	}
+	public String getCity(){
+		return textFieldCity.getText();
+	}
+	public String getState(){
+		return textFieldState.getText();
+	}
+	//NEED TO FIX TO IDENTIFY IF A CORRECT DATE WAS ENTERED
+	public String getDate(){
+		return textFieldDate.getText();
+	}
+}
